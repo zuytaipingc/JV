374efd023c8b44465974efcf31f27c5faadbf30c
zuytaipingc <627097073@qq.com>
2025-04-14 15:20:03 +0800

    更新登录和注册页面相关代码

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,50 @@
-// pages/login/login.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  //初始化数据
   data: {
-
+    //用户名
+    userName: '',
+    //密码
+    password: ''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  // 跳转到注册页面
+  register(){
+    wx.navigateTo({
+      url: '/pages/register/register',
+    })
   },
+  //登录
+  login() {
+    //去除空格
+    let data = {
+      userName: this.data.userName.replace(/\s+/g, ""),
+      password: this.data.password.replace(/\s+/g, ""),
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    }
+    //整理提交数据 添加校验判断值是否为空
+    //校验学号
+    if (!data.userName) {
+      wx.showToast({
+        title: '请输入学号',
+        icon:'none'
+      })
+      return
+    }
+    // 校验密码
+    if (!data.password) {
+      wx.showToast({
+        title: '请输入密码',
+        icon:'none'
+      })
+      return
+    }
+    //密码长度校验
+    if (data.password.length < 6) {
+      wx.showToast({
+        title: '密码不得少于6位',
+        icon:'none'
+      })
+      return
+    }
+    //发送请求
   }
 })
---
+++ pages/register/register.js
@@ -0,0 +1,20 @@
+Page({
+  //初始化数据
+  data:{
+    //部门
+    regDept:'',
+    // 学号
+    regCode:'',
+    // 姓名
+    regName:'',
+    // 手机号
+    regMobile:'',
+    // 密码
+    regPassword:''
+  },
+  // 注册
+  register(){
+    //非空校验
+    
+  }
+})
